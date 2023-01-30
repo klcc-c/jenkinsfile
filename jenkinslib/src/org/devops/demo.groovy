05afbc54890efa601b8d149382fd608c9789fbee
klcc-c <2564334707@qq.com>
2023-01-30 22:14:17 +0800

    d2

---
+++ jenkinslib/src/org/devops/demo.groovy
@@ -0,0 +1,17 @@
+package org.devops
+
+// GetUserName
+def GetUserNameByID(id){
+    users = [
+        ["id": 1, "name": "jenkins1"],
+        ["id": 2, "name": "jenkins2"],
+        ["id": 3, "name": "jenkins3"],
+    ]
+    
+    for (i in users){
+        if (i["id"] == id){
+            return i["name"]
+        }
+    }
+    return "null"
+}
